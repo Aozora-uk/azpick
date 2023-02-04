cd4e927cbaf43bd16773849d95166fbaaf4dbbac
morotesovtannu <kon2tannu@yandex.ru>
2023-02-05 01:23:58 +0900

    CookieClickerをなかったことにした

--- packages/client/src/widgets/index.ts
+++ packages/client/src/widgets/index.ts
@@ -23,7 +23,6 @@ export default function(app: App) {
 	app.component('MkwButton', defineAsyncComponent(() => import('./button.vue')));
 	app.component('MkwAiscript', defineAsyncComponent(() => import('./aiscript.vue')));
 	app.component('MkwAichan', defineAsyncComponent(() => import('./aichan.vue')));
-	app.component('MkwClicker', defineAsyncComponent(() => import('./clicker.vue')));
 }
 
 export const widgets = [
@@ -49,5 +48,4 @@ export const widgets = [
 	'button',
 	'aiscript',
 	'aichan',
-	'clicker',
 ];
