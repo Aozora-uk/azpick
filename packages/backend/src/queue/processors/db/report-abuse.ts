nekomiyanet/misskey-v12

feat: Abuse Report Resolver(backend)

packages/backend/src/queue/processors/db/report-abuse.ts
@@ -1,0 +1,95 @@
+import Bull from 'bull';
+import RE2 from 're2';
+import { queueLogger } from '../../logger.js';
+import { AbuseReportResolvers, AbuseUserReports, UserProfiles, Users } from '@/models/index.js';
+import { DbAbuseReportJobData } from '@/queue/types.js';
+import { MoreThan } from 'typeorm';
+import { sendEmail } from '@/services/send-email.js';
+import { emailDeliver, deliver } from '@/queue/index.js';
+import { getInstanceActor } from '@/services/instance-actor.js';
+import { publishAdminStream } from '@/services/stream.js';
+import { renderActivity } from '@/remote/activitypub/renderer/index.js';
+import { renderFlag } from '@/remote/activitypub/renderer/flag.js';
+import sanitizeHtml from 'sanitize-html';
+import { fetchMeta } from '@/misc/fetch-meta.js';
+
+const logger = queueLogger.createSubLogger('report-abuse');
+
+export async function reportAbuse(job: Bull.Job<DbAbuseReportJobData>): Promise<void> {
+	logger.info(`RUNNING...`);
+
+	const resolvers = await AbuseReportResolvers.find();
+
+	const targetUser = await Users.findOneOrFail({
+		id: job.data.targetUserId,
+	});
+
+	const reporter = await Users.findOneOrFail({
+		id: job.data.reporterId,
+	});
+
+	const actor = await getInstanceActor();
+
+	const targetUserAcct = targetUser.host ? `${targetUser.username.toLowerCase()}@${targetUser.host}` : targetUser.username.toLowerCase();
+
+	const reporterAcct = reporter.host ? `${reporter.username.toLowerCase()}@${reporter.host}` : reporter.username.toLowerCase();
+
+	for (const resolver of resolvers) {
+		if (!(resolver.targetUserPattern || resolver.reporterPattern || resolver.reportContentPattern)) {
+			continue;
+		}
+		const isTargetUserPatternMatched = resolver.targetUserPattern ? new RE2(resolver.targetUserPattern).test(targetUserAcct) : true;
+		const isReporterPatternMatched = resolver.reporterPattern ? new RE2(resolver.reporterPattern).test(reporterAcct) : true;
+		const isReportContentPatternMatched = resolver.reportContentPattern ? new RE2(resolver.reportContentPattern).test(job.data.comment) : true;
+		if (isTargetUserPatternMatched && isReporterPatternMatched && isReportContentPatternMatched) {
+			if (resolver.forward && job.data.targetUserHost !== null && job.data.reporterHost === null) {
+				deliver(actor, renderActivity(renderFlag(actor, [targetUser.uri!], report.comment)), targetUser.inbox);
+			}
+			await AbuseUserReports.update(job.data.id, {
+				resolved: true,
+				assigneeId: actor.id,
+				forwarded: resolver.forward && job.data.targetUserHost != null && job.data.reporterHost === null,
+			});
+			return;
+		}
+	}
+
+	// Publish event to moderators
+	setTimeout(async () => {
+		const moderators = await Users.find({
+			where: [{
+				isAdmin: true,
+			}, {
+				isModerator: true,
+			}],
+		});
+
+		for (const moderator of moderators) {
+			publishAdminStream(moderator.id, 'newAbuseUserReport', {
+				id: job.data.id,
+				targetUserId: job.data.targetUserId,
+				reporterId: job.data.reporterId,
+				comment: job.data.comment,
+			});
+
+			//const emailRecipientProfile = await UserProfiles.findOne({
+			//	userId: moderator.id,
+			//});
+
+			//if (emailRecipientProfile.email && emailRecipientProfile.emailVerified && emailRecipientProfile.receiveAnnouncementEmail) {
+			//	emailDeliver(emailRecipientProfile.email, 'New abuse report',
+			//		sanitizeHtml(job.data.comment),
+			//		sanitizeHtml(job.data.comment));
+			//}
+		}
+
+		const meta = await fetchMeta();
+		if ((meta.emailToReceiveAbuseReport || meta.email) && !meta.doNotSendNotificationEmailsForAbuseReport) {
+			emailDeliver(meta.emailToReceiveAbuseReport ?? meta.email!, 'New abuse report',
+				sanitizeHtml(job.data.comment),
+				sanitizeHtml(job.data.comment));
+		}
+	}, 1);
+
+	return 'Report resolved';
+}

packages/backend/src/remote/activitypub/kernel/flag/index.ts
@@ -4,6 +4,7 @@ import { IFlag, getApIds } from '../../type.js';
 import { AbuseUserReports, Users } from '@/models/index.js';
 import { In } from 'typeorm';
 import { genId } from '@/misc/gen-id.js';
+import { createReportAbuseJob } from '@/queue/index.js';
 
 export default async (actor: IRemoteUser, activity: IFlag): Promise<string> => {
 	// objectは `(User|Note) | (User|Note)[]` だけど、全パターンDBスキーマと対応させられないので
@@ -16,7 +17,7 @@ export default async (actor: IRemoteUser, activity: IFlag): Promise<string> => {
 	});
 	if (users.length < 1) return `skip`;
 
-	await AbuseUserReports.insert({
+	const report = await AbuseUserReports.insert({
 		id: genId(),
 		createdAt: new Date(),
 		targetUserId: users[0].id,
@@ -24,7 +25,9 @@ export default async (actor: IRemoteUser, activity: IFlag): Promise<string> => {
 		reporterId: actor.id,
 		reporterHost: actor.host,
 		comment: `${activity.content}\n${JSON.stringify(uris, null, 2)}`,
-	});
+	}).then(x => AbuseUserReports.findOneOrFail(x.identifiers[0]));
+
+	createReportAbuseJob(report);
 
 	return `ok`;
 };

packages/backend/src/server/api/endpoints/admin/abuse-report-resolver/create.ts
@@ -1,0 +1,113 @@
+import ms from 'ms';
+import define from '../../../define.js';
+import { AbuseReportResolvers } from '@/models/index.js';
+import { genId } from '@/misc/gen-id.js';
+import { ApiError } from '../../../error.js';
+
+export const meta = {
+	tags: ['admin'],
+
+	requireCredential: true,
+	requireModerator: true,
+
+	res: {
+		type: 'object',
+		optional: false, nullable: false,
+		properties: {
+			name: {
+				type: 'string',
+				nullable: false, optional: false,
+			},
+			targetUserPattern: {
+				type: 'string',
+				nullable: true, optional: false,
+			},
+			reporterPattern: {
+				type: 'string',
+				nullable: true, optional: false,
+			},
+			reportContentPattern: {
+				type: 'string',
+				nullable: true, optional: false,
+			},
+			expiresAt: {
+				type: 'string',
+				nullable: false, optional: false,
+				format: 'date-time',
+			},
+			forward: {
+				type: 'boolean',
+				nullable: false, optional: false,
+			},
+		},
+	},
+
+	errors: {
+		invalidRegularExpressionForTargetUser: {
+			message: 'Invalid regular expression for target user.',
+			code: 'INVALID_REGULAR_EXPRESSION_FOR_TARGET_USER',
+			id: 'c008484a-0a14-4e74-86f4-b176dc49fcaa',
+		},
+		invalidRegularExpressionForReporter: {
+			message: 'Invalid regular expression for reporter.',
+			code: 'INVALID_REGULAR_EXPRESSION_FOR_REPORTER',
+			id: '399b4062-257f-44c8-87cc-4ffae2527fbc',
+		},
+		invalidRegularExpressionForReportContent: {
+			message: 'Invalid regular expression for report content.',
+			code: 'INVALID_REGULAR_EXPRESSION_FOR_REPORT_CONTENT',
+			id: '88c124d8-f517-4c63-a464-0abc274168b',
+		},
+	},
+} as const;
+
+export const paramDef = {
+	type: 'object',
+	properties: {
+		name: { type: 'string', minLength: 1 },
+		targetUserPattern: { type: 'string', nullable: true },
+		reporterPattern: { type: 'string', nullable: true },
+		reportContentPattern: { type: 'string', nullable: true },
+		forward: { type: 'boolean' },
+	},
+	required: ['name', 'targetUserPattern', 'reporterPattern', 'reportContentPattern', 'forward'],
+} as const;
+
+// eslint-disable-next-line import/no-default-export
+export default define(meta, paramDef, async (ps, me) => {
+	if (ps.targetUserPattern) {
+		try {
+			new RegExp(ps.targetUserPattern);
+		} catch (e) {
+			throw new ApiError(meta.errors.invalidRegularExpressionForTargetUser);
+		}
+	}
+	if (ps.reporterPattern) {
+		try {
+			new RegExp(ps.reporterPattern);
+		} catch (e) {
+			throw new ApiError(meta.errors.invalidRegularExpressionForReporter);
+		}
+	}
+	if (ps.reportContentPattern) {
+		try {
+			new RegExp(ps.reportContentPattern);
+		} catch (e) {
+			throw new ApiError(meta.errors.invalidRegularExpressionForReportContent);
+		}
+	}
+	const now = new Date();
+
+	return await AbuseReportResolvers.insert({
+		id: genId(),
+		createdAt: now,
+		updatedAt: now,
+		name: ps.name,
+		targetUserPattern: ps.targetUserPattern,
+		reporterPattern: ps.reporterPattern,
+		reportContentPattern: ps.reportContentPattern,
+		expirationDate: null,
+		expiresAt: 'indefinitely',
+		forward: ps.forward,
+	}).then(x => AbuseReportResolvers.findOneOrFail(x.identifiers[0]));
+});

Aozora-uk/azpick

packages/backend/src/queue/processors/db/report-abuse.ts
@@ -1,0 +1,95 @@
+import Bull from 'bull';
+import RE2 from 're2';
+import { queueLogger } from '../../logger.js';
+import { AbuseReportResolvers, AbuseUserReports, UserProfiles, Users } from '@/models/index.js';
+import { DbAbuseReportJobData } from '@/queue/types.js';
+import { MoreThan } from 'typeorm';
+import { sendEmail } from '@/services/send-email.js';
+import { emailDeliver, deliver } from '@/queue/index.js';
+import { getInstanceActor } from '@/services/instance-actor.js';
+import { publishAdminStream } from '@/services/stream.js';
+import { renderActivity } from '@/remote/activitypub/renderer/index.js';
+import { renderFlag } from '@/remote/activitypub/renderer/flag.js';
+import sanitizeHtml from 'sanitize-html';
+import { fetchMeta } from '@/misc/fetch-meta.js';
+
+const logger = queueLogger.createSubLogger('report-abuse');
+
+export async function reportAbuse(job: Bull.Job<DbAbuseReportJobData>): Promise<void> {
+	logger.info(`RUNNING...`);
+
+	const resolvers = await AbuseReportResolvers.find();
+
+	const targetUser = await Users.findOneByOrFail({
+		id: job.data.targetUserId,
+	});
+
+	const reporter = await Users.findOneByOrFail({
+		id: job.data.reporterId,
+	});
+
+	const actor = await getInstanceActor();
+
+	const targetUserAcct = targetUser.host ? `${targetUser.username.toLowerCase()}@${targetUser.host}` : targetUser.username.toLowerCase();
+
+	const reporterAcct = reporter.host ? `${reporter.username.toLowerCase()}@${reporter.host}` : reporter.username.toLowerCase();
+
+	for (const resolver of resolvers) {
+		if (!(resolver.targetUserPattern || resolver.reporterPattern || resolver.reportContentPattern)) {
+			continue;
+		}
+		const isTargetUserPatternMatched = resolver.targetUserPattern ? new RE2(resolver.targetUserPattern).test(targetUserAcct) : true;
+		const isReporterPatternMatched = resolver.reporterPattern ? new RE2(resolver.reporterPattern).test(reporterAcct) : true;
+		const isReportContentPatternMatched = resolver.reportContentPattern ? new RE2(resolver.reportContentPattern).test(job.data.comment) : true;
+		if (isTargetUserPatternMatched && isReporterPatternMatched && isReportContentPatternMatched) {
+			if (resolver.forward && job.data.targetUserHost !== null && job.data.reporterHost === null) {
+				deliver(actor, renderActivity(renderFlag(actor, [targetUser.uri!], job.data.comment)), targetUser.inbox);
+			}
+			await AbuseUserReports.update(job.data.id, {
+				resolved: true,
+				assigneeId: actor.id,
+				forwarded: resolver.forward && job.data.targetUserHost != null && job.data.reporterHost === null,
+			});
+			return;
+		}
+	}
+
+	// Publish event to moderators
+	setTimeout(async () => {
+		const moderators = await Users.find({
+			where: [{
+				isAdmin: true,
+			}, {
+				isModerator: true,
+			}],
+		});
+
+		for (const moderator of moderators) {
+			publishAdminStream(moderator.id, 'newAbuseUserReport', {
+				id: job.data.id,
+				targetUserId: job.data.targetUserId,
+				reporterId: job.data.reporterId,
+				comment: job.data.comment,
+			});
+
+			//const emailRecipientProfile = await UserProfiles.findOneBy({
+			//	userId: moderator.id,
+			//});
+
+			//if (emailRecipientProfile.email && emailRecipientProfile.emailVerified && emailRecipientProfile.receiveAnnouncementEmail) {
+			//	emailDeliver(emailRecipientProfile.email, 'New abuse report',
+			//		sanitizeHtml(job.data.comment),
+			//		sanitizeHtml(job.data.comment));
+			//}
+		}
+
+		const meta = await fetchMeta();
+		if ((meta.emailToReceiveAbuseReport || meta.email) && !meta.doNotSendNotificationEmailsForAbuseReport) {
+			emailDeliver(meta.emailToReceiveAbuseReport ?? meta.email!, 'New abuse report',
+				sanitizeHtml(job.data.comment),
+				sanitizeHtml(job.data.comment));
+		}
+	}, 1);
+
+	return 'Report resolved';
+}

packages/backend/src/remote/activitypub/kernel/flag/index.ts
@@ -4,6 +4,7 @@ import { IFlag, getApIds } from '../../type.js';
 import { AbuseUserReports, Users } from '@/models/index.js';
 import { In } from 'typeorm';
 import { genId } from '@/misc/gen-id.js';
+import { createReportAbuseJob } from '@/queue/index.js';
 
 export default async (actor: CacheableRemoteUser, activity: IFlag): Promise<string> => {
 	// objectは `(User|Note) | (User|Note)[]` だけど、全パターンDBスキーマと対応させられないので
@@ -16,7 +17,7 @@ export default async (actor: CacheableRemoteUser, activity: IFlag): Promise<stri
 	});
 	if (users.length < 1) return `skip`;
 
-	await AbuseUserReports.insert({
+	const report = await AbuseUserReports.insert({
 		id: genId(),
 		createdAt: new Date(),
 		targetUserId: users[0].id,
@@ -24,7 +25,9 @@ export default async (actor: CacheableRemoteUser, activity: IFlag): Promise<stri
 		reporterId: actor.id,
 		reporterHost: actor.host,
 		comment: `${activity.content}\n${JSON.stringify(uris, null, 2)}`,
-	});
+	}).then(x => AbuseUserReports.findOneByOrFail(x.identifiers[0]));
+
+	createReportAbuseJob(report);
 
 	return `ok`;
 };

packages/backend/src/server/api/endpoints/admin/abuse-report-resolver/create.ts
@@ -1,0 +1,113 @@
+import ms from 'ms';
+import define from '../../../define.js';
+import { AbuseReportResolvers } from '@/models/index.js';
+import { genId } from '@/misc/gen-id.js';
+import { ApiError } from '../../../error.js';
+
+export const meta = {
+	tags: ['admin'],
+
+	requireCredential: true,
+	requireModerator: true,
+
+	res: {
+		type: 'object',
+		optional: false, nullable: false,
+		properties: {
+			name: {
+				type: 'string',
+				nullable: false, optional: false,
+			},
+			targetUserPattern: {
+				type: 'string',
+				nullable: true, optional: false,
+			},
+			reporterPattern: {
+				type: 'string',
+				nullable: true, optional: false,
+			},
+			reportContentPattern: {
+				type: 'string',
+				nullable: true, optional: false,
+			},
+			expiresAt: {
+				type: 'string',
+				nullable: false, optional: false,
+				format: 'date-time',
+			},
+			forward: {
+				type: 'boolean',
+				nullable: false, optional: false,
+			},
+		},
+	},
+
+	errors: {
+		invalidRegularExpressionForTargetUser: {
+			message: 'Invalid regular expression for target user.',
+			code: 'INVALID_REGULAR_EXPRESSION_FOR_TARGET_USER',
+			id: 'c008484a-0a14-4e74-86f4-b176dc49fcaa',
+		},
+		invalidRegularExpressionForReporter: {
+			message: 'Invalid regular expression for reporter.',
+			code: 'INVALID_REGULAR_EXPRESSION_FOR_REPORTER',
+			id: '399b4062-257f-44c8-87cc-4ffae2527fbc',
+		},
+		invalidRegularExpressionForReportContent: {
+			message: 'Invalid regular expression for report content.',
+			code: 'INVALID_REGULAR_EXPRESSION_FOR_REPORT_CONTENT',
+			id: '88c124d8-f517-4c63-a464-0abc274168b',
+		},
+	},
+} as const;
+
+export const paramDef = {
+	type: 'object',
+	properties: {
+		name: { type: 'string', minLength: 1 },
+		targetUserPattern: { type: 'string', nullable: true },
+		reporterPattern: { type: 'string', nullable: true },
+		reportContentPattern: { type: 'string', nullable: true },
+		forward: { type: 'boolean' },
+	},
+	required: ['name', 'targetUserPattern', 'reporterPattern', 'reportContentPattern', 'forward'],
+} as const;
+
+// eslint-disable-next-line import/no-default-export
+export default define(meta, paramDef, async (ps, me) => {
+	if (ps.targetUserPattern) {
+		try {
+			new RegExp(ps.targetUserPattern);
+		} catch (e) {
+			throw new ApiError(meta.errors.invalidRegularExpressionForTargetUser);
+		}
+	}
+	if (ps.reporterPattern) {
+		try {
+			new RegExp(ps.reporterPattern);
+		} catch (e) {
+			throw new ApiError(meta.errors.invalidRegularExpressionForReporter);
+		}
+	}
+	if (ps.reportContentPattern) {
+		try {
+			new RegExp(ps.reportContentPattern);
+		} catch (e) {
+			throw new ApiError(meta.errors.invalidRegularExpressionForReportContent);
+		}
+	}
+	const now = new Date();
+
+	return await AbuseReportResolvers.insert({
+		id: genId(),
+		createdAt: now,
+		updatedAt: now,
+		name: ps.name,
+		targetUserPattern: ps.targetUserPattern,
+		reporterPattern: ps.reporterPattern,
+		reportContentPattern: ps.reportContentPattern,
+		expirationDate: null,
+		expiresAt: 'indefinitely',
+		forward: ps.forward,
+	}).then(x => AbuseReportResolvers.findOneByOrFail(x.identifiers[0]));
+});